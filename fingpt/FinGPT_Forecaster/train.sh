17e179f44a24c36957f7387e25cf542cff1b36fa
Rinko Kobayakawa <zzforsaber9@gmail.com>
2023-12-28 17:10:08 +0800

    1

--- fingpt/FinGPT_Forecaster/train.sh
+++ fingpt/FinGPT_Forecaster/train.sh
@@ -5,7 +5,7 @@ export TOKENIZERS_PARALLELISM=0
 
 deepspeed \
 --include localhost:0,1 \
-train_lora.py \
+/FinGPT/fingpt/FinGPT_Forecaster/train_lora.py \
 --run_name dow30v3-llama2-5e-5lr-qkvogud \
 --base_model llama2 \
 --dataset dow30v3-2023-5-31_2023-11-30-llama \
